a0d362b593c27df45e71a456e8b5d2a231997b0a
Renate Gouveia <renate.gouveia@gmail.com>
2026-03-20 21:12:34 +0800

    Blank slate

--- apps/api/src/scripts/telegram-webhook-info.ts
+++
@@ -1,8 +0,0 @@
-import { loadEnvironment } from '../config/load-env.js';
-import { getTelegramWebhookInfo } from '../telegram/telegram-client.js';
-
-loadEnvironment(import.meta.url);
-
-const info = await getTelegramWebhookInfo();
-
-console.log(JSON.stringify(info, null, 2));
